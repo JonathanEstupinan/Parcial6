b71c40755fd29cb8a99e7dfa47ffa4f77bcfcb9c
Jonathan Estrada <jsestupinane1@academia.usbbog.edu.co>
2025-09-03 18:41:04 -0500

    primer commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 1, 121, 7)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -52,10 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // Lista de sonidos de emergencia (debes agregar los archivos en assets/audio/)
   final List<Map<String, String>> emergencySounds = [
-    {'label': 'Barcelona', 'file': 'Barcelona.mp3'},
-    {'label': 'Real madrid', 'file': 'Madrid.mp3'},
-    {'label': 'Liverpool', 'file': 'Liverpool.mp3'},
-    {'label': 'Bayern Munich', 'file': 'Bayern.mp3'},
+    {'label': 'Inicio', 'file': 'Inicio.mp4'},
+    {'label': 'Ingresa', 'file': 'Ingresa.PNG'},
+    {'label': 'Nosotros', 'file': 'Nosotros.PNG'},
+    {'label': 'Contactanos', 'file': 'Contactanos.PNG'},
+    {'label': 'Campo', 'file': 'Campo.mp3'},
   ];
 
   void _playSound(String fileName) async {
